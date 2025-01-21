5bf87a258a1e5594310885586bd2864d7649e46d
dauntss <kat.krahulec@gmail.com>
2025-01-20 17:55:43 -0700

    fixed env stuff

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const uri = 'mongodb+srv://xdantexxx:t6v5wdluFD6tp3rC@cluster0.fkek7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+mongoose.connect(process.env.MONGODB_URI || uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
