26a41d2a3ee9e304c6c828eeed8666d283ff8503
dauntss <kat.krahulec@gmail.com>
2025-01-20 18:31:42 -0700

    roll back some fixes

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
-const uri = 'mongodb+srv://xdantexxx:t6v5wdluFD6tp3rC@cluster0.fkek7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
-mongoose.connect(process.env.MONGODB_URI || uri, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
