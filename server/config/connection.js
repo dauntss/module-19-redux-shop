be0ae3edf33128d9f6f9acc3f89513382868e222
dauntss <kat.krahulec@gmail.com>
2025-01-21 19:37:01 -0700

    adding fixes for deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://xdantexxx:t6v5wdluFD6tp3rC@cluster0.fkek7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/reduxShop', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
